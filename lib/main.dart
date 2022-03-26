f7a9001a2b9fc7bb5c08011f95b7f0b80bf375de
Blake <blake@1024256.xyz>
2022-03-25 21:17:16 -0400

    More navigator pages!
    Signed-off-by: Blake <blake@1024256.xyz>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 //import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 void main() {
@@ -40,20 +41,71 @@ class OuterFrame extends StatelessWidget {
   Widget build(BuildContext context) {
     if (MediaQuery.of(context).size.width > 480) {
       return Row(children: [
-        Container(
-          width: 240,
-          alignment: Alignment.centerLeft,
-          color: Colors.green[300],
+        StatefulBuilder(
+          builder: (context, setState) {
+            return SizedBox(
+              width: 240,
+              child: Material(
+                child: NestedScrollView(
+                  headerSliverBuilder: (context, what) => [
+                    SliverToBoxAdapter(
+                      child: ListTile(
+                        leading: const Icon(MdiIcons.home, size: 36),
+                        title: const Text("Home"),
+                        selected: goRouter.location == "/",
+                        onTap: () {
+                          goRouter.go("/");
+                          setState(() {});
+                        },
+                      )
+                    ),
+                    SliverToBoxAdapter(
+                      child: ListTile(
+                        leading: const Icon(MdiIcons.musicBoxMultiple, size: 36),
+                        title: const Text("Library"),
+                        selected: goRouter.location.startsWith("/library"),
+                        onTap: () {
+                          goRouter.go("/library");
+                          setState(() {});
+                        },
+                      )
+                    )
+                  ],
+                  body: ListView()
+                ),
+              )
+            );
+          }
         ),
         Expanded(child: Builder(builder: (context) => buildNavigator(context)))
       ]);
     } else {
       return Column(children: [
         Expanded(child: Builder(builder: (context) => buildNavigator(context))),
-        Container(
-          height: 96,
-          alignment: Alignment.bottomCenter,
-          color: Colors.red[300],
+        StatefulBuilder(
+          builder: (context, setState) {
+            return BottomNavigationBar(
+              currentIndex: 
+              goRouter.location == "/" ? 0 :
+              goRouter.location.startsWith("/library") ? 1 :
+              goRouter.location == "/menu" ? 2 : -1,
+              items: const [
+                BottomNavigationBarItem(icon: Icon(MdiIcons.home), label: "Home"),
+                BottomNavigationBarItem(icon: Icon(MdiIcons.musicBoxMultiple), label: "Library"),
+                BottomNavigationBarItem(icon: Icon(MdiIcons.menu), label: "Menu")
+              ],
+              onTap: (i) {
+                if (i == 0) { // Home
+                  goRouter.go("/");
+                } else if (i == 1) { // Library
+                  goRouter.go("/library");
+                } else if (i == 2) {
+                  goRouter.push("/menu");
+                }
+                setState(() {});
+              },
+            );
+          }
         )
       ]);
     }
@@ -69,7 +121,9 @@ class OuterFrame extends StatelessWidget {
 
   static final goRouter = GoRouter(
     routes: [
-      GoRoute(path: "/", builder: (context, state) => const Scaffold(backgroundColor: Colors.red, body: const Center(child: const Text("HOME"))))
+      GoRoute(path: "/", builder: (context, state) => const Scaffold(backgroundColor: Colors.red, body: Center(child: Text("HOME")))),
+      GoRoute(path: "/library", builder: (context, state) => const Scaffold(backgroundColor: Colors.green, body: Center(child: Text("LIBRARY")))),
+      GoRoute(path: "/menu", builder: (context, state) => const Scaffold(backgroundColor: Colors.red, body: Center(child: Text("MAIN MENU"))))
     ]
   );
 }
