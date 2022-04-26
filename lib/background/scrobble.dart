fe67c0f3d7881b1c972d732edfcef4d63006fd31
Blake <blake@1024256.xyz>
2022-04-26 12:02:12 -0400

    Fix scrobble bug
    Signed-off-by: Blake <blake@1024256.xyz>

--- lib/background/scrobble.dart
+++ lib/background/scrobble.dart
@@ -1,22 +1,24 @@
-
-
-import 'package:audio_service/audio_service.dart';
 import 'package:bodacious/main.dart';
 import 'package:bodacious/src/online/lastfm.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:lastfm/lastfm.dart';
 
 Future<void> startScrobbling() async {
   String? last;
-  Future<void> scrobble() async {
+  Future<void> scrobble(Duration position) async {
     if (lastfm is LastFMAuthorized && config.lastFmToken != null) {
       if (player.mediaItem.valueOrNull?.artist == null) return;
       if (player.mediaItem.value?.id == last) return;
+      if (player.mediaItem.value?.duration == null) return;
       if (kDebugMode) {
-        print("Scrobbling...");
+        print("Scrobbling..."+(player.mediaItem.value?.id ?? ""));
       }
-      await (lastfm as LastFMAuthorized).scrobble(track: player.mediaItem.value!.title, artist: player.mediaItem.value!.artist!, album: player.mediaItem.value!.album!);
+      await (lastfm as LastFMAuthorized).scrobble(
+        track: player.mediaItem.value!.title,
+        artist: player.mediaItem.value!.artist!, 
+        album: player.mediaItem.value!.album!,
+        startTime: DateTime.now().toUtc().subtract(position)
+      );
       if (kDebugMode) {
         print("Scrobbling successful");
       }
@@ -28,10 +30,10 @@ Future<void> startScrobbling() async {
   await player.playbackState.listen((value) async {
     if (running) return;
     running = true;
-    if (value.position >= const Duration(minutes: 4)) {
-      await scrobble();
-    } else if (value.position.inSeconds >= (player.mediaItem.value?.duration ?? Duration.zero).inSeconds / 2) {
-      await scrobble();
+    if (player.position >= const Duration(minutes: 4)) {
+      await scrobble(player.position);
+    } else if (player.position.inSeconds >= (player.mediaItem.value?.duration ?? Duration.zero).inSeconds / 2) {
+      await scrobble(player.position);
     }
     running = false;
   }).asFuture();
@@ -44,7 +46,7 @@ Future<void> startLastFmNowPlaying() async {
       if (player.mediaItem.value?.id == last) return;
       if (player.mediaItem.valueOrNull?.artist == null) return;
       if (kDebugMode) {
-        print("Telling last.fm about our new song...");
+        print("Telling last.fm about our new song..."+(player.mediaItem.value?.id ?? ""));
       }
       await (lastfm as LastFMAuthorized).updateNowPlaying(track: player.mediaItem.value!.title, artist: player.mediaItem.value!.artist!, album: player.mediaItem.value!.album!);
       if (kDebugMode) {
@@ -58,6 +60,7 @@ Future<void> startLastFmNowPlaying() async {
   await player.mediaItem.listen((value) async {
     if (running) return;
     running = true;
+    await Future.delayed(const Duration(milliseconds: 500)); // mostly a workaround tbh
     await scrobble();
     running = false;
   }).asFuture();
