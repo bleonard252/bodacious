96378e9417667d5b069ec7f3721a3e52c8a4c847
Blake <blake@1024256.xyz>
2022-11-18 11:01:06 -0500

    Rebuild generated file

--- lib/models/playlist_data.freezed.dart
+++ lib/models/playlist_data.freezed.dart
@@ -61,7 +61,8 @@ mixin _$PlaylistMetadata {
 abstract class $PlaylistMetadataCopyWith<$Res> {
   factory $PlaylistMetadataCopyWith(
           PlaylistMetadata value, $Res Function(PlaylistMetadata) then) =
-      _$PlaylistMetadataCopyWithImpl<$Res>;
+      _$PlaylistMetadataCopyWithImpl<$Res, PlaylistMetadata>;
+  @useResult
   $Res call(
       {String id,
       String name,
@@ -75,64 +76,66 @@ abstract class $PlaylistMetadataCopyWith<$Res> {
 }
 
 /// @nodoc
-class _$PlaylistMetadataCopyWithImpl<$Res>
+class _$PlaylistMetadataCopyWithImpl<$Res, $Val extends PlaylistMetadata>
     implements $PlaylistMetadataCopyWith<$Res> {
   _$PlaylistMetadataCopyWithImpl(this._value, this._then);
 
-  final PlaylistMetadata _value;
   // ignore: unused_field
-  final $Res Function(PlaylistMetadata) _then;
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
 
+  @pragma('vm:prefer-inline')
   @override
   $Res call({
-    Object? id = freezed,
-    Object? name = freezed,
+    Object? id = null,
+    Object? name = null,
     Object? coverUri = freezed,
     Object? description = freezed,
-    Object? uri = freezed,
-    Object? trackList = freezed,
+    Object? uri = null,
+    Object? trackList = null,
     Object? document = freezed,
     Object? createdAt = freezed,
     Object? index = freezed,
   }) {
     return _then(_value.copyWith(
-      id: id == freezed
+      id: null == id
           ? _value.id
           : id // ignore: cast_nullable_to_non_nullable
               as String,
-      name: name == freezed
+      name: null == name
           ? _value.name
           : name // ignore: cast_nullable_to_non_nullable
               as String,
-      coverUri: coverUri == freezed
+      coverUri: freezed == coverUri
           ? _value.coverUri
           : coverUri // ignore: cast_nullable_to_non_nullable
               as Uri?,
-      description: description == freezed
+      description: freezed == description
           ? _value.description
           : description // ignore: cast_nullable_to_non_nullable
               as String?,
-      uri: uri == freezed
+      uri: null == uri
           ? _value.uri
           : uri // ignore: cast_nullable_to_non_nullable
               as Uri,
-      trackList: trackList == freezed
+      trackList: null == trackList
           ? _value.trackList
           : trackList // ignore: cast_nullable_to_non_nullable
               as List<String>,
-      document: document == freezed
+      document: freezed == document
           ? _value.document
           : document // ignore: cast_nullable_to_non_nullable
               as XmlDocument?,
-      createdAt: createdAt == freezed
+      createdAt: freezed == createdAt
           ? _value.createdAt
           : createdAt // ignore: cast_nullable_to_non_nullable
               as DateTime?,
-      index: index == freezed
+      index: freezed == index
           ? _value.index
           : index // ignore: cast_nullable_to_non_nullable
               as int?,
-    ));
+    ) as $Val);
   }
 }
 
@@ -143,6 +146,7 @@ abstract class _$$_PlaylistMetadataCopyWith<$Res>
           _$_PlaylistMetadata value, $Res Function(_$_PlaylistMetadata) then) =
       __$$_PlaylistMetadataCopyWithImpl<$Res>;
   @override
+  @useResult
   $Res call(
       {String id,
       String name,
@@ -157,61 +161,59 @@ abstract class _$$_PlaylistMetadataCopyWith<$Res>
 
 /// @nodoc
 class __$$_PlaylistMetadataCopyWithImpl<$Res>
-    extends _$PlaylistMetadataCopyWithImpl<$Res>
+    extends _$PlaylistMetadataCopyWithImpl<$Res, _$_PlaylistMetadata>
     implements _$$_PlaylistMetadataCopyWith<$Res> {
   __$$_PlaylistMetadataCopyWithImpl(
       _$_PlaylistMetadata _value, $Res Function(_$_PlaylistMetadata) _then)
-      : super(_value, (v) => _then(v as _$_PlaylistMetadata));
+      : super(_value, _then);
 
-  @override
-  _$_PlaylistMetadata get _value => super._value as _$_PlaylistMetadata;
-
+  @pragma('vm:prefer-inline')
   @override
   $Res call({
-    Object? id = freezed,
-    Object? name = freezed,
+    Object? id = null,
+    Object? name = null,
     Object? coverUri = freezed,
     Object? description = freezed,
-    Object? uri = freezed,
-    Object? trackList = freezed,
+    Object? uri = null,
+    Object? trackList = null,
     Object? document = freezed,
     Object? createdAt = freezed,
     Object? index = freezed,
   }) {
     return _then(_$_PlaylistMetadata(
-      id: id == freezed
+      id: null == id
           ? _value.id
           : id // ignore: cast_nullable_to_non_nullable
               as String,
-      name: name == freezed
+      name: null == name
           ? _value.name
           : name // ignore: cast_nullable_to_non_nullable
               as String,
-      coverUri: coverUri == freezed
+      coverUri: freezed == coverUri
           ? _value.coverUri
           : coverUri // ignore: cast_nullable_to_non_nullable
               as Uri?,
-      description: description == freezed
+      description: freezed == description
           ? _value.description
           : description // ignore: cast_nullable_to_non_nullable
               as String?,
-      uri: uri == freezed
+      uri: null == uri
           ? _value.uri
           : uri // ignore: cast_nullable_to_non_nullable
               as Uri,
-      trackList: trackList == freezed
+      trackList: null == trackList
           ? _value._trackList
           : trackList // ignore: cast_nullable_to_non_nullable
               as List<String>,
-      document: document == freezed
+      document: freezed == document
           ? _value.document
           : document // ignore: cast_nullable_to_non_nullable
               as XmlDocument?,
-      createdAt: createdAt == freezed
+      createdAt: freezed == createdAt
           ? _value.createdAt
           : createdAt // ignore: cast_nullable_to_non_nullable
               as DateTime?,
-      index: index == freezed
+      index: freezed == index
           ? _value.index
           : index // ignore: cast_nullable_to_non_nullable
               as int?,
@@ -314,41 +316,47 @@ class _$_PlaylistMetadata extends _PlaylistMetadata
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$_PlaylistMetadata &&
-            const DeepCollectionEquality().equals(other.id, id) &&
-            const DeepCollectionEquality().equals(other.name, name) &&
-            const DeepCollectionEquality().equals(other.coverUri, coverUri) &&
-            const DeepCollectionEquality()
-                .equals(other.description, description) &&
-            const DeepCollectionEquality().equals(other.uri, uri) &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.coverUri, coverUri) ||
+                other.coverUri == coverUri) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.uri, uri) || other.uri == uri) &&
             const DeepCollectionEquality()
                 .equals(other._trackList, _trackList) &&
-            const DeepCollectionEquality().equals(other.document, document) &&
-            const DeepCollectionEquality().equals(other.createdAt, createdAt) &&
-            const DeepCollectionEquality().equals(other.index, index));
+            (identical(other.document, document) ||
+                other.document == document) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt) &&
+            (identical(other.index, index) || other.index == index));
   }
 
   @JsonKey(ignore: true)
   @override
   int get hashCode => Object.hash(
       runtimeType,
-      const DeepCollectionEquality().hash(id),
-      const DeepCollectionEquality().hash(name),
-      const DeepCollectionEquality().hash(coverUri),
-      const DeepCollectionEquality().hash(description),
-      const DeepCollectionEquality().hash(uri),
+      id,
+      name,
+      coverUri,
+      description,
+      uri,
       const DeepCollectionEquality().hash(_trackList),
-      const DeepCollectionEquality().hash(document),
-      const DeepCollectionEquality().hash(createdAt),
-      const DeepCollectionEquality().hash(index));
+      document,
+      createdAt,
+      index);
 
   @JsonKey(ignore: true)
   @override
+  @pragma('vm:prefer-inline')
   _$$_PlaylistMetadataCopyWith<_$_PlaylistMetadata> get copyWith =>
       __$$_PlaylistMetadataCopyWithImpl<_$_PlaylistMetadata>(this, _$identity);
 
   @override
   Map<String, dynamic> toJson() {
-    return _$$_PlaylistMetadataToJson(this);
+    return _$$_PlaylistMetadataToJson(
+      this,
+    );
   }
 }
 
@@ -371,43 +379,43 @@ abstract class _PlaylistMetadata extends PlaylistMetadata {
   @override
 
   /// The database ID of the playlist.
-  String get id => throw _privateConstructorUsedError;
+  String get id;
   @override
 
   /// The user-defined name of this playlist.
   /// Defaults to the track, album, or artist used to create it.
-  String get name => throw _privateConstructorUsedError;
+  String get name;
   @override
 
   /// The URI to the cover. If not set, it defaults to:
   /// * no icon in the playlist view, AND
   /// * the first entry as the playlist icon in the list
-  Uri? get coverUri => throw _privateConstructorUsedError;
+  Uri? get coverUri;
   @override
-  String? get description => throw _privateConstructorUsedError;
+  String? get description;
   @override
 
   /// The URI to the XSPF file for this playlist.
-  Uri get uri => throw _privateConstructorUsedError;
+  Uri get uri;
   @override
 
   /// The URI of each track in the playlist, in order, as strings.
   /// Likely to be exactly how the <location/> contents appear.
-  List<String> get trackList => throw _privateConstructorUsedError;
+  List<String> get trackList;
   @override
 
   /// Attach the XML document to the object to easily get additional metadata
   /// from the file itself.
   @JsonKey(ignore: true)
-  XmlDocument? get document => throw _privateConstructorUsedError;
+  XmlDocument? get document;
   @override
 
   /// The playlist's declared creation date.
-  DateTime? get createdAt => throw _privateConstructorUsedError;
+  DateTime? get createdAt;
   @override
 
   /// The index of the playlist in the Library menu.
-  int? get index => throw _privateConstructorUsedError;
+  int? get index;
   @override
   @JsonKey(ignore: true)
   _$$_PlaylistMetadataCopyWith<_$_PlaylistMetadata> get copyWith =>
